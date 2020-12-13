0401326974e82770227f0b0660900a1515ca6b37
Norman Huang <normanh_@hotmail.com>
2020-12-13 13:47:52 -0500

    Revert "fit text on the contact email unit"

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,8 +6,7 @@ $(function() {
 	clientStuff();
 	
 	$("header h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' })
-  $(".biglink").fitText(1.5);
-  
+	
 });
 
 // smoothScroll function is applied from the document ready function
